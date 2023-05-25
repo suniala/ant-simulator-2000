952a594feba6cb10cfb5b7437f11afd5c9d85b16
Mikko Suniala <mikko.suniala@iki.fi>
2023-05-25 13:11:05 +0300

    Simple Canvas example

--- src/jvmMain/kotlin/ants/Main.kt
+++ src/jvmMain/kotlin/ants/Main.kt
@@ -1,27 +1,25 @@
 package ants
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            val canvasQuadrantSize = size / 2F
+            drawRect(
+                color = Color.Magenta,
+                size = canvasQuadrantSize
+            )
         }
     }
 }
