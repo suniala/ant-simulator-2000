5c59c7060b05f188dc2a10f373d428a27756269b
Mikko Suniala <mikko.suniala@iki.fi>
2023-05-26 21:16:16 +0300

    Switch to a simpler gradle project layout
    Based on:
    https://github.com/JetBrains/compose-multiplatform-desktop-template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,32 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("multiplatform")
+    kotlin("jvm")
     id("org.jetbrains.compose")
 }
 
-group = "fi.kapsi.kosmik.ants"
-version = "1.0-SNAPSHOT"
-
 repositories {
-    google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
-kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "17"
-        }
-        withJava()
+tasks.withType(KotlinCompile::class) {
+    kotlinOptions {
+        jvmTarget = "17"
     }
-    sourceSets {
-        val jvmMain by getting {
-            dependencies {
-                implementation(compose.desktop.currentOs)
-                implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
-            }
-        }
-        val jvmTest by getting
-    }
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
 }
 
 compose.desktop {
     application {
         mainClass = "ants.MainKt"
+
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "ant-simulator-2000"
