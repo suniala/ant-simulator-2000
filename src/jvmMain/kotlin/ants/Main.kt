19726ba0e9ca556fd873046451975e2837f0c191
Mikko Suniala <mikko.suniala@iki.fi>
2023-05-26 13:18:34 +0300

    Background task increments a counter, show counter in ui

--- src/jvmMain/kotlin/ants/Main.kt
+++ src/jvmMain/kotlin/ants/Main.kt
@@ -4,7 +4,9 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -17,41 +19,79 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
+import kotlin.system.measureTimeMillis
 
 @Composable
 @Preview
-fun App() {
+fun App(getCounter: () -> Int) {
     MaterialTheme {
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            val purpleColor = Color(0xFFBA68C8)
-            val worldSize = Size(1024f, 1024f)
-            drawCircle(
-                color = purpleColor,
-                radius = size.minDimension / 19f,
-                center = Offset(300f / worldSize.width * size.width, 400f / worldSize.height * size.height)
-            )
-            drawCircle(
-                color = purpleColor,
-                radius = size.minDimension / 19f,
-                center = Offset(600f / worldSize.width * size.width, 300f / worldSize.height * size.height)
-            )
-        }
+        Scaffold(
+            topBar = {
+                TopAppBar(title = {
+                    var counter by remember { mutableStateOf(0) }
+                    LaunchedEffect(counter) {
+                        delay(1_000)
+                        counter = getCounter()
+                    }
 
-        var text by remember { mutableStateOf("Hello, World!") }
-        LaunchedEffect(text) {
-            delay(1_000)
-            text = "$text."
+                    Text(text = counter.toString())
+                })
+            }
+        ) {
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                val purpleColor = Color(0xFFBA68C8)
+                val worldSize = Size(1024f, 1024f)
+                drawCircle(
+                    color = purpleColor,
+                    radius = size.minDimension / 19f,
+                    center = Offset(300f / worldSize.width * size.width, 400f / worldSize.height * size.height)
+                )
+                drawCircle(
+                    color = purpleColor,
+                    radius = size.minDimension / 19f,
+                    center = Offset(600f / worldSize.width * size.width, 300f / worldSize.height * size.height)
+                )
+            }
         }
-
-        Text(
-            text = text
-        )
     }
 }
 
+suspend fun massiveRun(action: suspend () -> Unit) {
+    val n = 131  // number of coroutines to launch
+    val k = 300 // times an action is repeated by each coroutine
+    val time = measureTimeMillis {
+        coroutineScope { // scope for coroutines
+            repeat(n) {
+                launch {
+                    repeat(k) { action() }
+                }
+            }
+        }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
+
+val counterContext = newSingleThreadContext("CounterContext")
+var counter = 0
+
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        App { counter }
+    }
+
+    LaunchedEffect(Unit) {
+        // confine everything to a single-threaded context
+        withContext(counterContext) {
+            massiveRun {
+                counter++
+                delay(111)
+            }
+        }
+        println("Counter = $counter")
     }
 }
